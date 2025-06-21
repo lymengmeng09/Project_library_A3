726cbbdfe682a3395e5c2b22a243c0f014cf3990
selatorm <sela.torm@student.passerellesnumeriques.org>
2025-06-21 09:51:11 +0700

    respone api laravel to vue

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,26 +1,40 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import { ref,onMounted } from 'vue';
+import axios from 'axios';
+// axios.js or in App.vue
+axios.defaults.baseURL = 'http://192.168.108.196:8000/api';
+
+
+  const books = ref([]);
+    onMounted(async ()=>{
+        {
+        try{
+            const res = await axios.get('http://192.168.108.11:8000/api/books')
+            books.value = res.data.data
+            console.log(res.data.data)
+        } catch(e){
+            console.log("fail")
+        }
+    }
+    })
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 v-for="book in books" :key="book.id">{{ book.title }}</h1>
 </template>
+
+
+
 
 <style scoped>
 header {
   line-height: 1.5;
 }
+
+
 
 .logo {
   display: block;
