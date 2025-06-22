1e1030a45d042833aecaf32937bb4fe03d66ea23
Sreyneath-Rom <sreyneath.rom@student.passerellesnumeriques.org>
2025-06-22 13:33:05 +0700

    add flexd to navber

--- Frontend/src/components/Navigation.vue
+++ Frontend/src/components/Navigation.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
+    <div class="bg-white shadow-md fixed top-0 left-0 right-0 z-50" >
         <!-- Sticky Navbar -->
-        <nav class="h-16 flex w-11/12 m-auto items-center justify-between sticky top-0 shadow-md z-50 bg-white">
+        <nav class="h-16 flex w-10/12 m-auto items-center justify-between ">
             <div id="nav-item" class="flex items-center gap-12">
                 <NavigationItem v-for="item in navItems" :key="item.label" :item="item" :to="item.path" />
             </div>
